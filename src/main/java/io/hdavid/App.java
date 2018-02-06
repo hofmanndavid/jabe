da7f19f1fb307b3ee83410b8d8641f6bdc0aa001
David Hofmann <hofmanndavid@gmail.com>
2018-02-06 18:04:17 -0300

    firsts tests attemting to embed vaadin in java se

--- src/main/java/io/hdavid/App.java
+++ src/main/java/io/hdavid/App.java
@@ -13,15 +13,24 @@ import io.undertow.servlet.api.ServletContainer;
 import io.undertow.servlet.handlers.DefaultServlet;
 import lombok.SneakyThrows;
 
+import javax.servlet.annotation.WebServlet;
+
 public class App {
 
     @SneakyThrows
     public static void main( String[] args ) {
+
         DeploymentInfo servletBuilder = Servlets.deployment()
                 .setClassLoader(App.class.getClassLoader())
-                .setContextPath("/myapp")
-                .setDeploymentName("test.war")
+                .setContextPath("/")
+                .setDeploymentName("jabe.war") // really need a name here?
+                .setDefaultSessionTimeout(60)
                 .addServlets(
+                        Servlets.servlet("VaadinUiServlet", VaadinUiServlet.class)
+//                                .addInitParam("vaadin.production", "false") // anotated in MuUI.java
+                                .setAsyncSupported(true)
+                                .addMapping("/app/*")
+                                .addMapping("/VAADIN/*"),
                         Servlets.servlet("MessageServlet", MessageServlet.class)
                                 .addInitParam("message", "Hello World")
                                 .addMapping("/sayhi"),
@@ -30,18 +39,17 @@ public class App {
                                 .addInitParam(DefaultServlet.DEFAULT_ALLOWED, "true")
                                 .addInitParam(DefaultServlet.ALLOW_POST, "false")
                                 .addInitParam(DefaultServlet.RESOLVE_AGAINST_CONTEXT_ROOT, "true"))
-
                 ;
-        servletBuilder.setResourceManager(new ClassPathResourceManager(App.class.getClassLoader(), "webapp"));
+
+        servletBuilder.setResourceManager(new ClassPathResourceManager(App.class.getClassLoader(), "webstatic"));
 
         ServletContainer servletContainer = Servlets.defaultContainer();
         DeploymentManager manager = servletContainer.addDeployment(servletBuilder);
         manager.deploy();
 
         HttpHandler start = manager.start();
-        PathHandler path = Handlers
-                .path(Handlers.redirect("/myapp"))
-                .addPrefixPath("/myapp", start);
+        PathHandler path = Handlers.path(Handlers.redirect("/"))
+                .addPrefixPath("/", start);
 
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
